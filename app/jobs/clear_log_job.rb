9de87d0902042e8dc03dfd8a66ceff67fedba514
Unknown <mg27ago@gmail.com>
2017-11-26 20:16:27 -0300

    Se borra el job para borrar logs
    Se borra el job para borrar logs

--- app/jobs/clear_log_job.rb
+++
@@ -1,9 +0,0 @@
-class ClearLogJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    puts "Start clear log"
-    File.delete('logFile') if File.exist?('logFile')
-    puts "Finally clear log"
-  end
-end
